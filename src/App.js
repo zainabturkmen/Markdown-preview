5b3cca3207d649c6ea879cd086a3b076736b3bd3
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-05-09 09:54:13 +0430

    markdown

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import ReactMarkdown from 'react-markdown'
 
 function App() {
   const [markdown, setMarkdown] = useState("## markdown preview")
+  return <main>
+    
+  </main>
 }
 
 export default App
